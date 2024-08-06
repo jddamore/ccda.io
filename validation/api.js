9823c2d07a89c4029d14d319886a46b37d90ee52
John DAmore <jddamore@gmail.com>
2024-08-06 09:39:13 -0400

    Adding an api path for ccda21, need to update schematron

--- validation/api.js
+++ validation/api.js
@@ -1,27 +1,37 @@
 const express = require("express");
 const app = express();
-const validate = require('./validate');
-const bodyParser = require('body-parser')
+const validate = require("./validate");
+const bodyParser = require("body-parser");
 
 let now = new Date().toISOString();
 
 let info = {
-  schemaVersion: 'CDA R2.0 from https://github.com/HL7/CDA-core-2.0 (retreived August 2024)',
-  schematronVersion: 'C-CDA R3.0 from https://github.com/HL7/CDA-ccda/tree/master/validation (retreived August 2024)',
-  limitations: 'This validation does not perform complete vocabulary checks and is based on schema/schematron above. THE SOFTWARE AND RESULTS ARE PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE',
-  onlineSince: now
-}
+  schemaVersion:
+    "CDA R2.0 from https://github.com/HL7/CDA-core-2.0 (retreived August 2024)",
+  schematronVersion:
+    "C-CDA R3.0 from https://github.com/HL7/CDA-ccda/tree/master/validation (retreived August 2024)",
+  limitations:
+    'This validation does not perform complete vocabulary checks and is based on schema/schematron above. THE SOFTWARE AND RESULTS ARE PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE',
+  onlineSince: now,
+};
 
-app.use(bodyParser.raw({ inflate: true, limit: '30mb', type: '*/*' }));
+let info21 = info
+info21.schematronVersion = 'C-CDA R2.1 with Companion Guide from https://github.com/HL7/CDA-ccda-2.1 and https://github.com/HL7/CDA-ccda-companion'
+
+app.use(bodyParser.raw({ inflate: true, limit: "30mb", type: "*/*" }));
 
 app.post("/validate", (req, res, next) => {
-  res.json(validate(req.body.toString(), info));
- });
+  res.json(validate.validate(req.body.toString(), info));
+});
+
+app.post("/validate21", (req, res, next) => {
+  res.json(validate.validate21(req.body.toString(), info));
+});
 
 app.get("/status", (req, res, next) => {
   res.json(info);
 });
 
 app.listen(80, () => {
- console.log("Server running on port 80");
+  console.log("Server running on port 80");
 });
--- validation/validate.js
+++ validation/validate.js
@@ -4,21 +4,25 @@ const schema = require("./schema-validator");
 const pd = require("pretty-data").pd;
 const chalk = require('chalk');
 
-var bad_xmlPath = "./examples/bad.xml";
-var good_xmlPath = "./examples/ccd_header.xml";
+var bad_xmlPath = "./examples/v3bad.xml";
+var good_xmlPath = "./examples/v3ccd_header.xml";
+var v21_xml = "./examples/v21validEmpty.xml"
 var schematronPath = "./ccda-3-0.sch";
+var v21schematronPath = './ccda-2-1.sch';
 
 var fs = require("fs");
 var bad_xml = fs.readFileSync(bad_xmlPath).toString();
 var good_xml = fs.readFileSync(good_xmlPath).toString();
+var v21good_xml = fs.readFileSync(v21_xml).toString();
 var schematron = fs.readFileSync(schematronPath).toString();
+var v21schematron = fs.readFileSync(v21schematronPath).toString();
 
 // edit schematron by replacing variables with actual codes
-let schema_json = JSON.parse(xml2json.toJson(schematron));
+let schematron_json = JSON.parse(xml2json.toJson(schematron));
 let schematron_variables = {};
-for (let i = 0; i < schema_json.schema.let.length; i++) {
-  schematron_variables[schema_json.schema.let[i].name] =
-    schema_json.schema.let[i].value;
+for (let i = 0; i < schematron_json.schema.let.length; i++) {
+  schematron_variables[schematron_json.schema.let[i].name] =
+  schematron_json.schema.let[i].value;
 }
 
 for (let key in schematron_variables) {
@@ -31,7 +35,7 @@ for (let key in schematron_variables) {
 // fs.writeFileSync('./new.sch', schematron);
 
 // check happy path
-console.log("testing a good xml ccd")
+console.log("testing a good 3.0 xml ccd")
 let schema_results = schema(good_xml);
 let schematron_results = validator.validate(good_xml, schematron);
 console.log(
@@ -42,12 +46,27 @@ console.log(
 );
 
 if (schema_results.pass !== true || schematron_results.errors.length) {
-  console.log(chalk.red('happy path failed'));
-  throw ('happy failure');
+  console.log(chalk.red('happy path 3.0 failed'));
+  throw ('happy 3.0 failure');
+}
+
+console.log("testing a good 2.1 xml ccd")
+schema_results = schema(good_xml);
+schematron_results = validator.validate(v21good_xml, v21schematron);
+console.log(
+  pd.json({
+    schema: schema_results,
+    schematron: schematron_results,
+  })
+);
+
+if (schema_results.pass !== true || schematron_results.errors.length) {
+  console.log(chalk.red('happy path 2.1 failed'));
+  throw ('happy 2.1 failure');
 }
 
 // check unhappy path
-console.log("testing a bad xml ccd")
+console.log("testing a bad 3.0 xml ccd")
 schema_results = schema(bad_xml);
 schematron_results = validator.validate(bad_xml, schematron);
 console.log(
@@ -58,17 +77,27 @@ console.log(
 );
 
 if (schema_results.pass !== false || !schematron_results.errors.length) {
-  console.log(chalk.red('unhappy path failed'));
-  throw ('unhappy failure');
+  console.log(chalk.red('unhappy path 3.0 failed'));
+  throw ('unhappy 3.0 failure');
 }
 else {
-  console.log(chalk.green("PASSING CHECKS: Happy and unhappy test passing!"))
+  console.log(chalk.green("PASSING CHECKS: Happy and unhappy tests passing!"))
 }
 
 // Note that server info is defined in api.js 
-module.exports = function (xml, info) {
+exports.validate = function (xml, info) {
   let schema_results = schema(xml);
   let schematron_results = validator.validate(xml, schematron);
+  return {
+      schema: schema_results,
+      schematron: schematron_results, 
+      info: info
+    }
+};
+
+exports.validate21 = function (xml, info) {
+  let schema_results = schema(xml);
+  let schematron_results = validator.validate(xml, v21schematron);
   return {
       schema: schema_results,
       schematron: schematron_results, 
