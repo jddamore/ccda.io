5e0d787f6ef023a2649c850d132fe817a143b0c5
John DAmore <jddamore@gmail.com>
2024-08-05 21:27:15 -0400

    Lots of styling on textarea, sync scroll

--- ui/src/validate.js
+++ ui/src/validate.js
@@ -1,25 +1,59 @@
-import React, { Component } from "react";
+import React, { Component, useRef} from "react";
 import Textarea from "@mui/joy/Textarea";
 import Button from "@mui/material/Button";
+import Switch from "@mui/material/Switch";
+import FormLabel from "@mui/material/FormLabel";
+// import FormControlLabel from "@mui/material/FormControlLabel";
 import SendIcon from "@mui/icons-material/Send";
 import DeleteIcon from "@mui/icons-material/Delete";
 import DriveFolderUploadIcon from "@mui/icons-material/DriveFolderUpload";
+import xmlFormat from "xml-formatter";
 
 export class Validate extends Component {
+
   state = {
-    content: ''
-  }
-  
-  style = {
-    marginLeft: "10%",
-    marginRight: "10%",
+    content: "",
+    v3: true,
+    lineCount: 1,
+    scrollTop: 0
   };
+
+  rows = 50;
+
+  container_style = {
+    marginLeft: "10%",
+    width: "80%",
+    marginRight: "10%",
+    float: "left",
+    display: "inline",
+  };
+
+  editor_style = {
+    fontFamily: "lucida console, courier new, courier, monospace",
+    fontSize: "14px",
+    lineHeight: "18px",
+    width: "95%"
+  }
 
   clear = () => {
     this.setState({
-      content: ''
-    })
-  }
+      content: "",
+      lineCount: 1,
+    });
+  };
+
+  schematron = (event) => {
+    // console.log(this.state)
+    if (event.target.checked) {
+      this.setState({
+        v3: true,
+      });
+    } else {
+      this.setState({
+        v3: false,
+      });
+    }
+  };
 
   handleChange = (event) => {
     event.preventDefault();
@@ -29,11 +63,32 @@ export class Validate extends Component {
 
     (async () => {
       const fileContent = await file.text();
+      let newFormat = xmlFormat(fileContent, {
+        indentation: "  ",
+        filter: (node) => node.type !== "Comment",
+        collapseContent: true,
+        lineSeparator: "\n",
+      });
+      let lineCount = newFormat.split("\n").length;
       this.setState({
-        content: fileContent
+        content: newFormat,
+        lineCount: lineCount,
       });
     })();
   };
+
+  createLineCount = () => {
+    let outarr = new Array();
+    for (var x = 0; x < this.state.lineCount; x++) {
+      outarr[x] = x + 1 + ".";
+    }
+    return outarr.join("\n");
+  };
+
+  matchScroll = (event) => {
+    let lineCounter1 = document.getElementById('line1');
+    lineCounter1.scrollTop = event.target.scrollTop 
+  }
 
   render() {
     return (
@@ -64,13 +119,46 @@ export class Validate extends Component {
           >
             Clear
           </Button>
+          Schematron Selection:
+          <FormLabel
+            style={{
+              marginLeft: "20px",
+              marginRight: "10px",
+              fontWeight: this.state.v3 ? 100 : 700,
+              color: this.state.v3 ? "lightgray" : "#1976D2",
+            }}
+          >
+            2.1
+          </FormLabel>
+          <Switch color="primary" defaultChecked onChange={this.schematron} />
+          <FormLabel
+            style={{
+              marginRight: "10px",
+              fontWeight: this.state.v3 ? 700 : 100,
+              color: this.state.v3 ? "#1976D2" : "lightgray",
+            }}
+          >
+            3.0
+          </FormLabel>
         </div>
-        <Textarea
-          maxRows={30}
-          placeholder="Paste XML (or click 'upload from file')"
-          style={this.style}
-          value = {this.state.content}
-        />
+        <div style={this.container_style}>
+          <textarea
+            id="line1"
+            className="lineCounter"
+            rows={this.rows}
+            style={{ float: "left" }}
+            readOnly
+            value={this.createLineCount(this.state.lineCount)}
+          />
+          <textarea
+            className="codeArea"
+            rows={this.rows}
+            placeholder="Paste XML (or click 'upload from file')"
+            value={this.state.content}
+            style={this.editor_style}
+            onScroll={this.matchScroll}
+          />
+        </div>
       </div>
     );
   }
